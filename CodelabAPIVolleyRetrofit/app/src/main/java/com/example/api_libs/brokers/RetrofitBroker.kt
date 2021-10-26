0fe087da8a59595b6f1675869325947846186c7f
Camilo <ca.escobar2434@uniandes.edu.co>
2021-10-26 12:07:15 +0200

    CodelabAPIVolleyRetrofit

---
+++ CodelabAPIVolleyRetrofit/app/src/main/java/com/example/api_libs/brokers/RetrofitBroker.kt
@@ -0,0 +1,55 @@
+package com.example.api_libs.brokers
+
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class RetrofitBroker {
+    companion object{
+        var test:String = "holaaaaa"
+        fun getRequest(onResponse:(resp:String)->Unit, onFailure:(resp:String)->Unit) {
+            var r = RetrofitApi.retrofitService.getProperties()
+            var p = r.enqueue(
+                object : Callback<String> {
+                    override fun onFailure(call: Call<String>, t: Throwable) {
+                        onFailure(t.message!!)
+                    }
+
+                    override fun onResponse(call: Call<String>, response: Response<String>) {
+                        onResponse(response.body()!!)
+                    }
+                })
+        }
+        fun postRequest(body: Map<String, String>, onResponse:(resp:String)->Unit, onFailure:(resp:String)->Unit) : String? {
+            var resp: String? = null
+            Log.d("tag3", "entras")
+
+            RetrofitApi.retrofitService.postProperties(body["name"] ?: "", body["telephone"] ?: "", body["email"] ?:"").enqueue(
+                object : Callback<String> {
+                    override fun onFailure(call: Call<String>, t: Throwable) {
+                        onFailure(t.message!!)
+                    }
+
+                    override fun onResponse(call: Call<String>, response: Response<String>) {
+                        onResponse(response.body()!!)
+                    }
+                })
+            return resp
+        }
+        fun putRequest(body: Map<String, String>, onResponse:(resp:String)->Unit, onFailure:(resp:String)->Unit) : String? {
+            var resp: String? = null
+            RetrofitApi.retrofitService.putProperties(body[""] ?:"", body["name"] ?: "", body["telephone"] ?: "", body["email"] ?:"").enqueue(
+                object : Callback<String> {
+                    override fun onFailure(call: Call<String>, t: Throwable) {
+                        onFailure(t.message!!)
+                    }
+
+                    override fun onResponse(call: Call<String>, response: Response<String>) {
+                        onResponse(response.body()!!)
+                    }
+                })
+            return resp
+        }
+    }
+}
